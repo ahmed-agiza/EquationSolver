4748f24bace67bf3a8c043ba4e82cfc550545f6a
Ahmed Agiza <ahmed.agiza22@gmail.com>
2015-04-14 17:07:32 +0200

    The Jacobi Method

--- SimultaneousEquationsSolver/SimultaneousEquationsSolver/main.cpp
+++ SimultaneousEquationsSolver/SimultaneousEquationsSolver/main.cpp
@@ -39,15 +39,15 @@ public:
 };
 
 //----Matrix Class----------
-struct Row4 {
+struct Row4 { //Row Struct
 	double element[4];
 };
 
-struct Column3 {
+struct Column3 { //Column Struct
 	double element[3];
 };
 
-Row4 operator+(Row4 rowA, Row4 rowB) {
+Row4 operator+(Row4 rowA, Row4 rowB) { //Adding 2 Rows Operator.
 	Row4 result;
 	result.element[0] = rowA.element[0] + rowB.element[0];
 	result.element[1] = rowA.element[1] + rowB.element[1];
@@ -56,7 +56,7 @@ Row4 operator+(Row4 rowA, Row4 rowB) {
 	return result;
 }
 
-Row4 operator*(Row4 rowA, double scale) {
+Row4 operator*(Row4 rowA, double scale) { //Multiplying Row by Scalar Operator.
 	Row4 result;
 	result.element[0] = scale * rowA.element[0];
 	result.element[1] = scale * rowA.element[1];
@@ -66,11 +66,11 @@ Row4 operator*(Row4 rowA, double scale) {
 }
 
 
-class Matrix3x4 {
+class Matrix3x4 { //Matrix Class.
 	Row4 rows[3];
 
 public:
-	Matrix3x4(double a00 = 0, double a01 = 0, double a02 = 0, double a03 = 0,
+	Matrix3x4(double a00 = 0, double a01 = 0, double a02 = 0, double a03 = 0, //Constructor.
 		double a10 = 0, double a11 = 0, double a12 = 0, double a13 = 0,
 		double a20 = 0, double a21 = 0, double a22 = 0, double a23 = 0) {
 		rows[0].element[0] = a00; rows[0].element[1] = a01; rows[0].element[2] = a02; rows[0].element[3] = a03;
@@ -78,59 +78,59 @@ public:
 		rows[2].element[0] = a20; rows[2].element[1] = a21; rows[2].element[2] = a22; rows[2].element[3] = a23;
 	}
 	
-	Row4 addRow(int indexA, int indexB, double scale = 1.0) {
+	Row4 addRow(int indexA, int indexB, double scale = 1.0) { //Adding two rows.
 		return (rows[indexA] = rows[indexA] + (rows[indexB] * scale));
 	}
 
-	Column3 getColumn(int index) const {
+	Column3 getColumn(int index) const { //Getting column by index.
 		Column3 column;
 		for (int i = 0; i < 3; i++)
 			column.element[i] = rows[i].element[index];
 		return column;
 	}
 
-	double getElement(int row, int column) const {
+	double getElement(int row, int column) const { //Getting element at any location.
 		return rows[row].element[column];
 	}
 
-	double getPivot(int index) const{
+	double getPivot(int index) const{ //Getting row pivot.
 		return rows[index].element[index];
 	}
 
-	void multiplyRow(int index, double scale) {
+	void multiplyRow(int index, double scale) { //Multiplying row by scalar.
 		rows[index] = rows[index] * scale;
 	}
 	
-	void setRow(int index, Row4 row) {
+	void setRow(int index, Row4 row) { //Assigning row to row.
 		rows[index] = row;
 	}
 	
-	void setRow(int index, double a0 = 0.0, double a1 = 0.0, double a2 = 0.0, double a3 = 0.0) {
+	void setRow(int index, double a0 = 0.0, double a1 = 0.0, double a2 = 0.0, double a3 = 0.0) { //Assigning elements to row.
 		rows[index].element[0] = a0;
 		rows[index].element[1] = a1;
 		rows[index].element[2] = a2;
 		rows[index].element[3] = a3;
 	}
 	
-	void setElement(int row, int column, double value = 0.0) {
+	void setElement(int row, int column, double value = 0.0) { //Setting element at certain location.
 		rows[row].element[column] = value;
 	}
 
-	void swapRows(int rowA, int rowB) {
+	void swapRows(int rowA, int rowB) { //Swapping two rows.
 		Row4 temp = rows[rowA];
 		rows[rowA] = rows[rowB];
 		rows[rowB] = temp;
 	}
 
-	bool hasZeroDiagonalElement() const {
+	bool hasZeroDiagonalElement() const { //Checking if the matrix has zero's at the diagonal.
 		return (rows[0].element[0] == 0 || rows[1].element[1] == 0 || rows[2].element[2] == 0);
 	}
 
-	bool hasZeroDiagonalElement(int index) const {
+	bool hasZeroDiagonalElement(int index) const { //Checking if the pivot of a row is zero.
 		return rows[index].element[index] == 0;
 	}
 
-	void eliminateZeroDiagonalElements() {
+	void eliminateZeroDiagonalElements() { //Eliminating zero element diagonals.
 		Matrix3x4 original = *this;
 		double iterations = 0;
 		if (hasZeroDiagonalElement(0)){
@@ -153,7 +153,7 @@ public:
 		}
 	}
 
-	bool isReducedEchelonForm() const {
+	bool isReducedEchelonForm() const { //Checking if the matrix is at Reduced Row Echelon Form.
 		for (int i = 0; i < 4; i++)
 			for (int j = 0; j < 4; j++)
 				if (j == i){
@@ -166,11 +166,11 @@ public:
 		return true;
 	}
 
-	Row4 getRow(int index) const {
+	Row4 getRow(int index) const { //Getting row at certain index.
 		return rows[index];
 	}
 
-	void removeEpsilons() {
+	void removeEpsilons() { //Remove elements less than epislons.
 		for (int i = 0; i < 3; i++){
 			for (int j = 0; j < 4; j++)
 				if ((rows[i].element[j] <= numeric_limits<double>::epsilon() * 10.0 && rows[i].element[j] > 0)
@@ -179,7 +179,7 @@ public:
 		}
 	}
 
-	void normalizePivots() {
+	void normalizePivots() { //Normalizing row pivots.
 		for (int i = 0; i < 3; i++){
 			double pivot = getPivot(i);
 			if (pivot == 0)
@@ -189,12 +189,12 @@ public:
 	}
 };
 
-ostream &operator<<(ostream &stream, const Row4 &row) {
+ostream &operator<<(ostream &stream, const Row4 &row) { //Printing Row Operator.
 	stream << row.element[0] << "\t" << row.element[1] << "\t" << row.element[2] << "\t" << row.element[3] << endl;
 	return stream;
 }
 
-ostream &operator<<(ostream &stream, const Matrix3x4 &matrix) {
+ostream &operator<<(ostream &stream, const Matrix3x4 &matrix) { //Printing Matrix Operator.
 	Row4 matrixRows[3];
 	matrixRows[0] = matrix.getRow(0);
 	matrixRows[1] = matrix.getRow(1);
@@ -218,6 +218,7 @@ typedef function<double(double, double)> Equation;
 
 //----Helper Functions----------
 void initEquations();
+bool validSolution(Equation e0, Equation e1, Equation e2, double x0, double x1, double x2);
 void displayMethodsMenu();
 void displayExitMenu();
 int getSelection(int min, int max);
@@ -252,7 +253,7 @@ int main(int argc, char **argv) {
 					break;
 				case JACOBI:
 					cout << "Solving using the Jacobi Method: " << endl;
-					solution = findRootByJacobi(equations[0], equations[1], equations[2]); //Compute the root using the Jacobi Method.
+					solution = findRootByJacobi(equations[1], equations[2], equations[3]); //Compute the root using the Jacobi Method.
 					break;
 				
 				}
@@ -287,7 +288,7 @@ int main(int argc, char **argv) {
 
 void initEquations() {
 	equations[1] = [](double x1, double x2) {
-		return ((27 - 2 * x1 + x2) / 10.0); //x0 Equation.
+		return ((27.0 - 2.0 * x1 + x2) / 10.0); //x0 Equation.
 	};
 	equations[2] = [](double x0, double x2) {
 		return ((-61.5 + 3 * x0 - 2 * x2) / -6.0); //x1 Equation.
@@ -296,9 +297,9 @@ void initEquations() {
 		return ((-21.5 - x0 - x1) / 5.0); //x2 Equation.
 	};
 
-	coefficients = Matrix3x4(10,  2, -1, 27, //Coefficients Matrix.
-							 -3, -6,  2, -61.5,
-							  1,  1,  5, -21.5); 
+	coefficients = Matrix3x4(10,  2.0, -1.0, 27.0, //Coefficients Matrix.
+							 -3.0, -6.0,  2.0, -61.5,
+							  1.0,  1.0,  5.0, -21.5); 
 }
 
 void displayMethodsMenu() { //Printing Method Selection Menu.
@@ -338,6 +339,10 @@ int getSelection(int min, int max) { //Geting User Selected Option.
 	return atoi(buf);
 }
 
+bool validSolution(Equation e0, Equation e1, Equation e2, double x0, double x1, double x2) { //Checking if the solution is valid.
+	return (x0 == e0(x1, x2) && x1 == e1(x0, x2) && x2 == e2(x0, x1));
+}
+
 double getMaxError(double a0, double a1, double a2) { //Getting the maximum error.
 	if (a0 > a1){
 		if (a0 > a2)
@@ -354,10 +359,10 @@ double getMaxError(double a0, double a1, double a2) { //Getting the maximum erro
 Solution findRootByGauss(Matrix3x4 coeff) { //Computing the solution using Gauss-Jordan Elimination Method.
 	Solution solution;
 	solution.x0 = 0; solution.x1 = 0; solution.x2 = 0; solution.error = 0; solution.iterations = 0;
-	coeff.eliminateZeroDiagonalElements();
-	for (int i = 0; i < 3; i++){
-		double pivot = coeff.getPivot(i);
-		for (int j = 0; j < 3; j++){
+	coeff.eliminateZeroDiagonalElements(); //Eliminating zero elements at diagonal.
+	for (int i = 0; i < 3; i++){ //Elimination loop.
+		double pivot = coeff.getPivot(i); //Getting row pivot.
+		for (int j = 0; j < 3; j++){ //----Eliminating below and above rows--------
 			if (i == j)
 				continue;
 			double numerator = coeff.getElement(j, i);
@@ -366,8 +371,9 @@ Solution findRootByGauss(Matrix3x4 coeff) { //Computing the solution using Gauss
 			coeff.removeEpsilons();
 		}
 	}
-	coeff.normalizePivots();
-	Column3 solutionColumn = coeff.getColumn(3);
+
+	coeff.normalizePivots(); //Normalizing pivots.
+	Column3 solutionColumn = coeff.getColumn(3); //Getting solution vector.
 	solution.x0 = solutionColumn.element[0];
 	solution.x1 = solutionColumn.element[1];
 	solution.x2 = solutionColumn.element[2];
@@ -378,5 +384,42 @@ Solution findRootByJacobi(Equation e0, Equation e1, Equation e2) { //Computing t
 	Solution solution;
 	solution.x0 = 0; solution.x1 = 0; solution.x2 = 0; solution.error = 0; solution.iterations = 0;
 
+	double x0 = 0, x1 = 0, x2 = 0, //Initial guesses.
+		   x0n = 0, x1n = 0, x2n = 0, //Next guesses.
+		   err0 = DBL_MAX, err1 = DBL_MAX, err2 = DBL_MAX, maxErr = DBL_MAX; //Errors.
+
+	int iterations = 0; //Iterations count.
+
+	while (maxErr > EPSILON && iterations < MAX_ITERATIONS && !validSolution(e0, e1, e2, x0, x1, x2)){ //ITerations loop.
+		x0n = e0(x1, x2); //Computing next x0.
+		x1n = e1(x0, x2); //Computing next x1.
+		x2n = e2(x0, x1); //Computing next x2.
+
+		err0 = (x0 - x0n) / x0n; //Computing x0 error.
+		if (err0 < 0)
+			err0 *= -1.0;
+
+		err1 = (x1 - x1n) / x1n; //Computing x1 error.
+		if (err1 < 0)
+			err1 *= -1.0;
+
+		err2 = (x2 - x2n) / x2n; //Computing x2 error.
+		if (err2 < 0)
+			err2 *= -1.0;
+
+		maxErr = getMaxError(err0, err1, err2); //Computing maximum error.
+
+		x0 = x0n;
+		x1 = x1n;
+		x2 = x2n;
+		iterations++;
+	}
+
+	solution.x0 = x0n;
+	solution.x1 = x1n;
+	solution.x2 = x2n;
+	solution.error = maxErr;
+	solution.iterations = iterations;
 	return solution;
 }
+
